2313ac4e6a633e1604a84b60a2798dbfb3bfbfa8
Ezgi <ezgiiioe@87-77-93-98.mna.fu-berlin.de>
2024-08-27 11:56:58 +0200

    Test Test

--- youtube.py
+++ youtube.py
@@ -1,19 +1,18 @@
 import yt_dlp
 
-# URL des YouTube-Videos
+# URL des YouTube-Videos (Beispiel)
 url = "https://youtu.be/1-7bIxu_z0s?si=-g5LrUtWL6mMGxsT"
 
 # Optionen zum Herunterladen festlegen
 ydl_opts = {
     'format': 'best',
     'outtmpl': '%(title)s.%(ext)s',
-    'socket_timeout': 60,  # Timeout auf 60 Sekunden setzen
+    'socket_timeout': 60,  # Timeout auf 60 Sekunden setzen, da sonst Fehlermeldung!
 }
 
 # Herunterladen des Videos
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url])
-
 
 
 
